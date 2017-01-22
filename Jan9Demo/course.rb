4fb31b3c82959678e6058d185a33f36e874ab8d0
squiggsnet <squiggs.rafael@gmail.com>
2017-01-22 14:40:55 -0400

    added logger

--- Jan9Demo/course.rb
+++ Jan9Demo/course.rb
@@ -1,12 +1,17 @@
+require 'logger'
 require_relative 'student'
 
 class Course
   attr_reader :course_code
   attr_reader :studentOne
+  attr_reader :logger
 
   #constructor for course class
   def initialize(cCode)
     @course_code = cCode
+    #@logger = Logger.new(STDOUT)
+    @logger = Logger.new('logFile.txt')
+    @logger.level = Logger::DEBUG
   end
 
   #add student to class
@@ -18,7 +23,9 @@ class Course
   if __FILE__ == $0 then
     puts "The class size limit is " + Student.MAX_CLASS_SIZE.to_s
     myCourse = Course.new("WEBD3101")
+
     myCourse.addStudent("w8584021","Squiggs", "Net")
+    myCourse.logger.info("Added new Student ID: "+ "w8584021")
 
     puts "The student list for " + myCourse.course_code
     puts myCourse.studentOne.reportInfo
