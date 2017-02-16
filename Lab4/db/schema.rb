294113627551554ac41e89f7947fd89ecc212d12
squiggsnet <squiggs.rafael@gmail.com>
2017-02-16 19:21:28 -0400

    added devise gem and set up authentication

--- Lab4/db/schema.rb
+++ Lab4/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170216230739) do
 
   create_table "Album", primary_key: "AlbumId", force: :cascade do |t|
     t.string  "Title",    limit: 160, null: false
@@ -119,6 +119,23 @@ ActiveRecord::Schema.define(version: 0) do
     t.index ["GenreId"], name: "IFK_TrackGenreId"
     t.index ["MediaTypeId"], name: "IFK_TrackMediaTypeId"
     t.index ["TrackId"], name: "IPK_Track", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
